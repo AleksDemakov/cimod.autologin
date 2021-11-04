cdcbb21286b106a818f9d342551e2e5b6055d11b
Aleksey Demakov <60163292+AleksDemakov@users.noreply.github.com>
2021-11-04 08:37:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	document.getElementsByName("nik")[0]["value"]="user"
+	document.getElementsByName("password")[0]["value"]="password"
+	document.getElementsByTagName("input")[2].click();
+});
+
